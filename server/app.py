3fd4961de3efb4ad3ad50ebf250f8dd5f2734c2f
Abdisalan Ibrahim <crgmelo22@gmail.com>
2025-01-14 18:39:35 +0300

    .

--- server/app.py
+++ server/app.py
@@ -1,5 +1,16 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return '<h1>Welcome to the EpicTech Home Page!</h1>'
+
+
+@app.route('/EpicTech')
+def epic_tech():
+    return '<h1>Welcome to EpicTech!</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
